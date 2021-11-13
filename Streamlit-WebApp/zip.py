de8ea338f027427cff2193903de4628ac7920186
honghusldl <84426827+honghusldl@users.noreply.github.com>
2021-11-13 15:53:17 -0500

    Update zip.py

--- Streamlit-WebApp/zip.py
+++ Streamlit-WebApp/zip.py
@@ -1,7 +1,20 @@
 import zipfile
 
-with zipfile.ZipFile('19-22 Data.zip','w',compression=zipfile.ZIP_DEFLATED) as my_zip:
-    my_zip.write('2019data.xlsx')
-    my_zip.write('2020data.xlsx')
-    my_zip.write('2021data.xlsx')
-    my_zip.write('2022data.xlsx')
+# zip
+# with zipfile.ZipFile('19-22 Data.zip','w',compression=zipfile.ZIP_DEFLATED) as my_zip:
+#     my_zip.write('2019data.xlsx')
+#     my_zip.write('2020data.xlsx')
+#     my_zip.write('2021data.xlsx')
+#     my_zip.write('2022data.xlsx')
+
+# extract
+# with zipfile.ZipFile('19-22 Data.zip','r') as my_zip:
+#     #print(my_zip.namelist())
+#     my_zip.extractall('Data')
+
+# zip
+import shutil
+# shutil.make_archive('data','zip','Data')
+
+# extract
+shutil.unpack_archive('data.zip','data2')
